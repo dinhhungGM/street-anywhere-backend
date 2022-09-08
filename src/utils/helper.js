a3426992d595f9a6f9c273fbd4442e059641b411
Linhttn1000 <18521000@gm.uit.edu.vn>
2022-09-08 14:56:56 +0700

    add mock data

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,8 +1,30 @@
+const bcryptjs = require('bcryptjs');
+const _ = require('lodash');
+
 module.exports = {
-  createError: (status, statusCode, message) => {
+  createError: (statusCode, message) => {
     const error = new Error(message);
-    error.status = status;
     error.statusCode = statusCode;
     return error;
-  }
+  },
+  hashPassword: async (password) => {
+    try {
+      const hash = await bcryptjs.hash(password, 10);
+      return hash;
+    } catch (error) {
+      console.log('Error: Hash password');
+      throw error;
+    }
+  },
+  isPasswordMatch: async (password, hash) => {
+    try {
+      return await bcryptjs.compare(password, hash);
+    } catch (error) {
+      console.log('Error: Compare password');
+      throw error;
+    }
+  },
+  destruct: (exclusiveProps, obj) => {
+    return _.omit(obj, exclusiveProps);
+  },
 };
