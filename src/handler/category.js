1209dbc7473d7948b2e0190d5db44e87b6cfa699
Linhttn1000 <18521000@gm.uit.edu.vn>
2022-09-08 21:50:57 +0700

    create end point for creating new categories

--- src/handler/category.js
+++ src/handler/category.js
@@ -1,2 +1,26 @@
+const { Op } = require('sequelize');
 const catchAsync = require('./../utils/catchAsync');
-const helper = require('./../utils/catchAsync');
+const helper = require('./../utils/helper');
+const { category: Category } = require('./../models');
+
+module.exports = {
+  createNewCategory: catchAsync(async (req, res) => {
+    const { categoryName } = req.body;
+    const existedCategories = await Category.findAll({
+      raw: true,
+      where: {
+        categoryName: {
+          [Op.iLike]: categoryName,
+        },
+      },
+    });
+    if (existedCategories.length) {
+      throw helper.createError(400, 'Category was existed. Please choose another category');
+    }
+    await Category.create({ categoryName });
+    return res.status(201).json({
+      status: 'Success',
+      message: 'Create a new category successfully',
+    });
+  }),
+};
--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,2 +1,15 @@
 const router = require('express').Router();
+const { body } = require('express-validator');
+const CategoryHandler = require('./../handler/category');
+const ErrorHandler = require('./../handler/error');
+
+router
+  .route('')
+  .post(
+    body('categoryName', 'Category name does not empty').not().isEmpty(),
+    body('categoryName', 'Category name have at most 50 characters').isLength({ max: 50 }),
+    ErrorHandler.catchValidationError,
+    CategoryHandler.createNewCategory,
+  );
+
 module.exports = router;
