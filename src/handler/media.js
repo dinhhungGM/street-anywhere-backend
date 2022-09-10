82ccecf3d1d7dc529c57b07e7cefe353bd83e885
Linhttn1000 <18521000@gm.uit.edu.vn>
2022-09-10 11:51:17 +0700

    update media routes

--- src/handler/media.js
+++ src/handler/media.js
@@ -1,2 +1,14 @@
 const catchAsync = require('./../utils/catchAsync');
-const helper = require('./../utils/catchAsync');
+const helper = require('./../utils/helper');
+const { mediaSource: MediaSource } = require('./../models');
+
+module.exports = {
+  getMedia: catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const mediaSource = await MediaSource.findByPk(+id);
+    if (!mediaSource) {
+      throw helper.createError(404, 'No media source found');
+    }
+    return res.header('Content-Type', 'image/jpeg').status(200).send(mediaSource.sources);
+  }),
+};
--- src/routes/mediaRoutes.js
+++ src/routes/mediaRoutes.js
@@ -1,2 +1,12 @@
 const router = require('express').Router();
+const { param } = require('express-validator');
+const MediaHandler = require('./../handler/media');
+const ErrorHandler = require('./../handler/error');
+
+router.get(
+  '/:id',
+  param('id', 'Media Source Id does not empty').exists().isNumeric().isInt(),
+  ErrorHandler.catchValidationError,
+  MediaHandler.getMedia,
+);
 module.exports = router;
