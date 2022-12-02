f8e2e902ca09c04969012059c5c2e7192fb6ac49
duyphuong02 <18521276@gm.uit.edu.vn>
2022-12-02 21:32:08 +0700

    reconfig deployment

---
+++ src/config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+module.exports = {
+  development: {
+    username: process.env.DEV_USERNAME,
+    password: process.env.DEV_PASSWORD,
+    database: process.env.DEV_DATABASE,
+    host: process.env.DEV_HOST,
+    port: process.env.DEV_PORT,
+    dialect: 'postgres',
+    logging: true,
+  },
+  production: {
+    username: process.env.PROD_USERNAME,
+    password: process.env.PROD_PASSWORD,
+    database: process.env.PROD_DATABASE,
+    host: process.env.PROD_HOST,
+    dialect: 'postgres',
+    port: process.env.PROD_PORT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    use_env_variable: 'PROD_CONNECTION_STRING',
+  },
+};
